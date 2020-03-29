08d16ca13fa4ca51b29316762061b8e7f666e906
wzdnn <62141495+wzdnn@users.noreply.github.com>
2020-03-29 17:56:16 +0700

    penjumlahan method.cpp

---
+++ tambah github.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+void tambah(){
+	int a,b;
+	
+	cout << "Masukkan angka pertama : ";
+	cin  >> a;
+	
+	cout << "Masukkan angka kedua   : ";
+	cin  >> b;
+	
+	cout << "Hasil Penjumlahan : " << a+b;
+}
+
+main ()
+{
+	tambah();
+}
